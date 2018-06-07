ec9ab8ab9c00057ca062065915ddec3963fb4d8a
Garlic Xu <spurs-xu@qq.com>
2018-06-07 08:57:40 +0800

    Add an annotated query to repository

--- src/main/java/application/repository/HomeWorkRepository.java
+++ src/main/java/application/repository/HomeWorkRepository.java
@@ -1,7 +1,10 @@
 package application.repository;
 
 import application.entity.HomeWork;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Set;
 
 /**
  * Creator: DreamBoy
@@ -9,4 +12,8 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface HomeWorkRepository extends CrudRepository<HomeWork, Long> {
     HomeWork findById(long id);
+
+    @Query("MATCH (map: MindMap)-[:hasRootNode]->(root:Node)-[:hasChild*]->(child:Node)," +
+            "(hw: HomeWork)<-[:hasHomeWork]-(n) WHERE n IN [root, child] RETURN hw")
+    Set<HomeWork> findByMindMapId(long id);
 }
