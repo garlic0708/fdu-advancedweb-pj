923086fe81fa32d326b825fb83b7019b31f48d4a
Garlic Xu <spurs-xu@qq.com>
2018-06-09 01:25:07 +0800

    Fixed bug of mock data service

--- src/frontend/src/app/mock-provider.service.ts
+++ src/frontend/src/app/mock-provider.service.ts
@@ -4,16 +4,17 @@ import {Observable} from "rxjs/index";
 import {mockData} from "../assets/mock-data";
 import * as _ from 'lodash';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class MockProviderService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(req.url);
     const key = `${req.method} ${req.url}`;
     const mockDatum = mockData[
       Object.keys(mockData).find(k => {
-        k = k.replace(/:[A-Za-z]*(\/|$)/, "\\d+");
-        return new RegExp(_.escapeRegExp(k)).test(key);
+        k = _.escapeRegExp(k);
+        k = k.replace(/:[A-Za-z]*(\\\/|$)/, "\\d+$1");
+        console.log(k, new RegExp(k), key);
+        return new RegExp(k).test(key);
       })];
     const response = {
       status: 200,
